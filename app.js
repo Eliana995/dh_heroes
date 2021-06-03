4ea054bfe0c3daae8103e0df94e93190fea41f5f
Eliana995 <eliana.ag.95@gmail.com>
2021-06-03 00:14:49 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require('path');
+const views = path.join(__dirname, 'views');
+
+
+app.get('/', (req,res) => res.sendFile(path.join(views, 'home.html')));
+app.get('/badtz-maru', (req,res) => res.sendFile(path.join(views, 'badtz-maru.html')));
+app.get('/chococat', (req,res) => res.sendFile(path.join(views, 'chococat.html')));
+app.get('/keroppi', (req,res) => res.sendFile(path.join(views, 'keroppi.html')));
+app.get('/my_melody', (req,res) => res.sendFile(path.join(views, 'my_melody.html')));
+app.get('/kuromi', (req,res) => res.sendFile(path.join(views, 'kuromi.html')));
+
+
+app.use(express.static('public'))
+
+
+app.listen(port, () => { console.log('Servidor corriendo en el puerto ' + port); })
